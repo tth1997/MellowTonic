b6730d69ff7febbe471fbafedd7a981c1e349ceb
ZacharySs <53802794+ZacharySs@users.noreply.github.com>
2019-08-14 12:56:52 +1000

    IGB200 Project Folders
    Initial project upload

---
+++ Assets/Scripts/Vehicles/CarMovementScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovementScript : MonoBehaviour
+{
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    void Steer()
+    {
+        steeringAngle = maxSteerAngle * horizontalInput;
+        frontLeftW.steerAngle = steeringAngle;
+        frontRightW.steerAngle = steeringAngle;
+    }
+
+    void Accelerate()
+    {
+        frontLeftW.motorTorque = verticalInput * motorForce;
+        frontRightW.motorTorque = verticalInput * motorForce;
+    }
+
+    void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontLeftW, frontLeftT);
+        UpdateWheelPose(frontRightW, frontRightT);
+        UpdateWheelPose(rearLeftW, rearLeftT);
+        UpdateWheelPose(rearRightW, rearRightT);
+    }
+
+    void UpdateWheelPose(WheelCollider collider, Transform Wtransform)
+    {
+        Vector3 pos = Wtransform.position;
+        Quaternion quat = Wtransform.rotation;
+
+        collider.GetWorldPose(out pos, out quat);
+
+        Wtransform.position = pos;
+        Wtransform.rotation = quat;
+    }
+
+    float horizontalInput;
+    float verticalInput;
+    float steeringAngle;
+
+    public WheelCollider frontLeftW, frontRightW;
+    public WheelCollider rearLeftW, rearRightW;
+    public Transform frontLeftT, frontRightT;
+    public Transform rearLeftT, rearRightT;
+    public float maxSteerAngle = 30f;
+    public float motorForce = 50f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+}
