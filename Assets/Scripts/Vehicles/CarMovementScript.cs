7015e183bb2f1e57517024afc24838c1e3463cb7
ZacharySs <zacsyntageros@gmail.com>
2019-08-18 21:46:07 +1000

    - Fixed menu button interaction issue; EventSystem was missing in MainMenu scene - Added placeholder car model (grey sedan), but this will have to be replaced in future, as the interior isnt modelled - Car has a working prototype physics model in place, which includes:   - WheelColliders with tuned settings (please dont touch), through which the car turn left/right, accelerate/reverse and brake (currently it's WASD + Spacebar for brake)   - Preliminary input delay (manually adjusted in Unity editor). Works as advertised and essentially causes a delay between when a key is pressed and when the car reacts to that key press. Input delay currently only applies to left/right steering.   - Variable left/right steering input sensitivity; as the car increases in speed, the steering sensitivity decreases to allow for smoother steering and to avoid instantly flipping the car over. However, prolonged steerin in one direction at high speed will still cause the car to flip (this is intentional).

--- Assets/Scripts/Vehicles/CarMovementScript.cs
+++ Assets/Scripts/Vehicles/CarMovementScript.cs
@@ -1,13 +1,152 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CarMovementScript : MonoBehaviour
 {
+
+    float horizontalInput;
+    float verticalInput;
+    float steeringAngle;
+
+    public WheelCollider frontLeftW, frontRightW;
+    public WheelCollider rearLeftW, rearRightW;
+    public Transform frontLeftT, frontRightT;
+    public Transform rearLeftT, rearRightT;
+    Rigidbody carRB;
+
+    float maxSteerAngle = 30f;
+    public float inputDelay;
+    public float horizInputSens = 0.2f;
+    public float horizInputGrav = 1f;
+    float inputSensModerator;
+    float steeringTarget;
+
+    bool gettingInput;
+    float motorForce = 1000f;
+    float brakeForce = 50000f;
+    [SerializeField]
+    float carVel;
+
+    public Text speedText;
+    public Text verticalAxisText;
+
+    void Start()
+    {
+        carRB = GetComponent<Rigidbody>();
+
+        StartCoroutine(InputDelay());
+    }
+
+    void Update()
+    {
+        GetInput();
+
+        carVel = carRB.velocity.magnitude;
+
+        if (speedText != null)
+            speedText.text = Mathf.Round(carVel * 3.6f).ToString() + "km/h";
+
+        if (verticalAxisText != null)
+            verticalAxisText.text = "VertAxis: " + Input.GetAxis("Vertical").ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        Steer();
+        Accelerate();
+        //UpdateWheelPoses();
+    }
+
+    IEnumerator InputDelay()
+    {
+        while (true)
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                yield return new WaitForSeconds(inputDelay);
+                steeringTarget = 1f;
+                Debug.Log("Input delayed by " + inputDelay);
+
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                yield return new WaitForSeconds(inputDelay);
+                steeringTarget = -1f;
+                Debug.Log("Input delayed by " + inputDelay);
+            }
+            else
+            {
+                steeringTarget = 0f;
+            }
+            yield return null;
+        }
+    }
     void GetInput()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+
         verticalInput = Input.GetAxis("Vertical");
+
+        inputSensModerator = Mathf.Clamp(5 / carVel, 0, 1);
+
+        if (steeringTarget > 0)
+        {
+            if (horizontalInput >= 0)
+            {
+                horizontalInput = Mathf.Clamp(horizontalInput + horizInputSens * inputSensModerator * Time.deltaTime,
+                                              -1f,
+                                               1f);
+            }
+            else
+            {
+                horizontalInput = Mathf.Clamp(horizontalInput + horizInputGrav * inputSensModerator * Time.deltaTime,
+                            -1f,
+                            1f);
+            }
+        }
+        else if (steeringTarget < 0)
+        {
+            if (horizontalInput <= 0)
+            {
+                horizontalInput = Mathf.Clamp(horizontalInput - horizInputSens * inputSensModerator * Time.deltaTime,
+                            -1f,
+                            1f);
+            }
+            else
+            {
+                horizontalInput = Mathf.Clamp(horizontalInput - horizInputGrav * inputSensModerator * Time.deltaTime,
+                            -1f,
+                            1f);
+            }
+        }
+        else
+        {
+            if (horizontalInput < 0)
+            {
+                horizontalInput = Mathf.Clamp(horizontalInput + horizInputGrav * inputSensModerator * Time.deltaTime,
+                            -1f,
+                            0f);
+            }
+            else if (horizontalInput > 0)
+            {
+                horizontalInput = Mathf.Clamp(horizontalInput - horizInputGrav * inputSensModerator * Time.deltaTime,
+                            0f,
+                            1f);
+            }
+        }
+
+        // Braking
+        if (Input.GetKey(KeyCode.Space))
+        {
+            frontLeftW.brakeTorque = brakeForce;
+            frontRightW.brakeTorque = brakeForce;
+        }
+        else
+        {
+            frontLeftW.brakeTorque = 0f;
+            frontRightW.brakeTorque = 0f;
+        }
     }
 
     void Steer()
@@ -38,36 +177,9 @@ public class CarMovementScript : MonoBehaviour
 
         collider.GetWorldPose(out pos, out quat);
 
+        //quat.eulerAngles = new Vector3(quat.eulerAngles.x, Wtransform.rotation.eulerAngles.y, Wtransform.rotation.eulerAngles.z);
+
         Wtransform.position = pos;
-        Wtransform.rotation = quat;
-    }
-
-    float horizontalInput;
-    float verticalInput;
-    float steeringAngle;
-
-    public WheelCollider frontLeftW, frontRightW;
-    public WheelCollider rearLeftW, rearRightW;
-    public Transform frontLeftT, frontRightT;
-    public Transform rearLeftT, rearRightT;
-    public float maxSteerAngle = 30f;
-    public float motorForce = 50f;
-
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-
-    }
-
-    private void FixedUpdate()
-    {
-        GetInput();
-        Steer();
-        Accelerate();
-        UpdateWheelPoses();
+        Wtransform.localRotation = quat;
     }
 }
