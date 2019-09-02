06295d3c1466b2e24ad396998f35a5b56171a355
Joel Parsons <n10147241@qut.edu.au>
2019-09-02 18:29:48 +1000

    Menu System
    Menu buttons working, Instruction screen and name change

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+   
+    public void Instructions()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
