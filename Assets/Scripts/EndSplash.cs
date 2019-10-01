370f47db9d100827775f9338c027b2a0ae135eea
Joel Parsons <n10147241@qut.edu.au>
2019-10-01 15:36:26 +1000

    End State
    End state - Audio and flashing lights

---
+++ Assets/Scripts/EndSplash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class EndSplash : MonoBehaviour
+{
+    public float wait_time = 45f;
+
+    void Start()
+    {
+        StartCoroutine(Wait_for_intro());
+    }
+
+    IEnumerator Wait_for_intro()
+    {
+        yield return new WaitForSeconds(wait_time);
+
+        SceneManager.LoadScene(0);
+    }
+}
+
