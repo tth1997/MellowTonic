1523d062bc78f402f9258d440b4109dc045cc39b
ZacharySs <zacsyntageros@gmail.com>
2019-10-06 21:45:09 +1000

    - Changes to road layout again (making sure roads are same height, cleaner fits between prefabs, cleaner rest stop area, etc.) - Working AI car and NavMesh. PLEASE NOTE: The NavMesh setup uses transparent blocks placed below road lanes (named A1, A2, B1, B2). These have been organised in such a way that an AI car can navigate with precision along a specific highway lane. Please make sure that these blocks connect to each other when changing/adding anything to the highway layout, otherwise this will cause problems with the NavMesh upon re-baking. - Fixed an issue where, after exiting freelook, the camera would not snap to its original relative rotation within the car.

---
+++ Assets/Scripts/Vehicles/AI Car/AICarMovementScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class AICarMovementScript : MonoBehaviour
+{
+    [HideInInspector]
+    public NavMeshAgent carAgent;
+
+    [HideInInspector]
+    public Transform waypointCurrent;
+
+    int waypointNum = 1; // Waypoint 1 corresponds to A lanes, 2 to B lanes
+
+    // FUTURE REFERENCE:
+    // Spawn an AI car every 30 - 60 seconds
+    // The AI's "Lane Number" is randomised between 1 and 4. 1-2 = A lanes, 3-4 = B lanes.
+    // Lane Number will determine initial orientation of the vehicle as well as which lane it will use to navigate
+    // AI car speed is also randomised between 90 and 110kph.
+    // If the AI car speed (upon instantiation) is LOWER than the player's speed, it will spawn AHEAD of the player by ~300m.
+    // Likewise, if the AI car speed is HIGHER than the player's, it will spawn BEHIND the player.
+    // If an AI car is more than 500m away from the player, despawn it.
+
+    void Start()
+    {
+        carAgent = GetComponent<NavMeshAgent>();
+
+        waypointCurrent = GameObject.FindGameObjectWithTag("Waypoint" + waypointNum.ToString()).transform;        
+    }
+
+    void Update()
+    {
+        if (waypointCurrent != null)
+        {
+            carAgent.SetDestination(waypointCurrent.position);
+        }
+        else if (waypointCurrent == null)
+        {
+            Debug.Log("Waypoint A not found!");
+        }
+    }
+}
---
+++ Assets/Scripts/Vehicles/Player Car/MouseController.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public Vector3 rigOffset;
+    public Transform playerCarTransform;
+    public Transform mouseAimTransform;
+    public Transform camRigTransform;
+    public Transform cameraTransform;
+    float angleLimit = 150f;
+
+    public bool paused;
+    public bool relativeControl;
+
+    float mouseX;
+    float mouseY;
+    float mouseAimSmoothCoef = 10f;
+
+    float snapMouseX;
+    float snapMouseY;
+    float snapAngleX;
+    float snapAngleY;
+
+    [SerializeField]
+    float camSmoothSpeed = 3f;
+    float mouseSens = 0.5f;
+    float aimDistance = 200f;
+
+    /// <summary>
+    /// Get a point projected out to aimDistance meters along the forward direction of the player's car.
+    /// </summary>
+    public Vector3 ForwardAimPos
+    {
+        get
+        {
+            /// if 'playerCarTransform' is null, then '?' will return; otherwise, ':' will be returned
+            return playerCarTransform == null
+                ? transform.forward * aimDistance
+                : (playerCarTransform.forward * aimDistance) + playerCarTransform.position;
+        }
+    }
+
+    /// <summary>
+    /// Get the position that the mouse is indicating the car should drive towards, projected
+    /// out to aimDistance meters. Also meant to be used to draw a mouse cursor.
+    /// </summary>
+    public Vector3 MouseAimPos
+    {
+        get
+        {
+            return mouseAimTransform == null
+                ? transform.forward * aimDistance
+                : (mouseAimTransform.forward * aimDistance) + mouseAimTransform.position;
+        }
+    }
+
+    /// <summary>
+    /// The desired rotation angle of the mouseAimTransform (in degrees).
+    /// </summary>
+    public Vector2 LookAngle
+    {
+        get
+        {
+            mouseX += Input.GetAxis("Mouse X") * mouseSens;
+            mouseY += -Input.GetAxis("Mouse Y") * mouseSens;
+
+            if (Input.GetKeyDown(KeyCode.Mouse1))
+            {
+                snapAngleX = mouseAimTransform.localEulerAngles.x;
+                snapAngleY = mouseAimTransform.localEulerAngles.y;
+            }
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                mouseAimTransform.localEulerAngles = new Vector3(snapAngleX, snapAngleY, 0);
+
+                mouseX = mouseAimTransform.eulerAngles.y;
+                mouseY = mouseAimTransform.eulerAngles.x;
+            }
+
+            if (Mathf.DeltaAngle(transform.eulerAngles.y, mouseX) > angleLimit)
+            {
+                mouseX -= Mathf.DeltaAngle(transform.eulerAngles.y, mouseX) - angleLimit;
+            }
+            else if (Mathf.DeltaAngle(transform.eulerAngles.y, mouseX) < -angleLimit)
+            {
+                mouseX -= Mathf.DeltaAngle(transform.eulerAngles.y, mouseX) + angleLimit;
+            }
+
+            return new Vector2(mouseY, mouseX);
+        }
+    }
+
+    private void Awake()
+    {
+        if (relativeControl)
+        {
+            mouseY = mouseAimTransform.localEulerAngles.x;
+            mouseX = mouseAimTransform.localEulerAngles.y;
+        }
+        else
+        {
+            mouseY = mouseAimTransform.eulerAngles.x;
+            mouseX = mouseAimTransform.eulerAngles.y;
+        }
+
+    }
+
+    private void Start()
+    {
+        playerCarTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        if (!playerCarTransform)
+            Debug.LogError("playerCarTransform is not specified. Make sure that the PlayerCar prefab is in the scene.");
+        else
+            transform.localPosition = rigOffset;
+    }
+
+    void Update()
+    {
+        if (!paused)
+        {
+            RotateRig();
+        }
+    }
+
+    private void RotateRig()
+    {
+        if (mouseAimTransform == null || cameraTransform == null || camRigTransform == null)
+            return;
+
+        if (relativeControl)
+        {
+            mouseAimTransform.localEulerAngles = new Vector3(Mathf.LerpAngle(mouseAimTransform.localEulerAngles.x, LookAngle.x, mouseAimSmoothCoef),
+                                                             Mathf.LerpAngle(mouseAimTransform.localEulerAngles.y, LookAngle.y, mouseAimSmoothCoef),
+                                                             mouseAimTransform.localEulerAngles.z);
+        }
+        else
+        {
+            mouseAimTransform.eulerAngles = new Vector3(Mathf.LerpAngle(mouseAimTransform.eulerAngles.x, LookAngle.x, mouseAimSmoothCoef),
+                                                        Mathf.LerpAngle(mouseAimTransform.eulerAngles.y, LookAngle.y, mouseAimSmoothCoef),
+                                                        mouseAimTransform.eulerAngles.z);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!paused)
+        {
+            RotateCamera();
+        }
+    }
+
+    void RotateCamera()
+    {
+        // The up vector of the camera normally is aligned to the horizon. However, when
+        // looking straight up/down this can feel a bit weird. At those extremes, the camera
+        // stops aligning to the horizon and instead aligns to itself.
+        Vector3 upVec = (Mathf.Abs(mouseAimTransform.forward.y) > 0.99f)
+            ? camRigTransform.up
+            : Vector3.up;
+
+        // Smoothly rotate the camera to face the mouse aim.
+        camRigTransform.rotation = Damp(camRigTransform.rotation,
+                                        Quaternion.LookRotation(MouseAimPos - camRigTransform.position, upVec),
+                                        camSmoothSpeed,
+                                        Time.deltaTime);
+    }
+
+    private Quaternion Damp(Quaternion a, Quaternion b, float lambda, float dt)
+    {
+        return Quaternion.Slerp(a, b, 1 - Mathf.Exp(-lambda * dt));
+    }
+}
