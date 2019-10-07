2bded72c9c1f5217464f792ce998427c277a6699
louiscbirch <louis.c.birch@hotmail.com>
2019-10-07 17:03:12 +1000

    - added tutorial system

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public bool tutorial;
+    public int tutorialCount;
+    public GameObject tutorialText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        tutorialCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "TutorialTrigger")
+        {
+            tutorialText.SetActive(true);
+        }        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "TutorialTrigger")
+        {
+            tutorialText.SetActive(false);
+            tutorialCount++;
+            Debug.Log(tutorialCount);
+        }
+    }
+}
