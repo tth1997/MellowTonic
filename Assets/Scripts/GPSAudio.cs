4c7ffe1957d9e97c48b3ef18120ebbe73a01549a
Joel Parsons <n10147241@qut.edu.au>
2019-10-07 13:37:16 +1000

    GPS Audio
    GPS trigger and signs placed throughout level

---
+++ Assets/Scripts/GPSAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSAudio : MonoBehaviour
+{
+    public AudioSource GPSSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GPSSource = GetComponent<AudioSource> ();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PlayerCar")
+        {
+
+            GPSSource.Play();
+        }
+    }
+}
