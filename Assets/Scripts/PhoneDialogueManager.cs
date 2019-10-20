e0c5fbdb0d2a0eaaac57a6b1e87cc42f21b7853b
tth17 <angrydice@gmail.com>
2019-10-21 01:17:23 +1000

    - Implemented a Dialogue manager to phone dialogues (still needs revision) - Fixed the Tutorial Text not fading with the raw image.

--- Assets/Scripts/PhoneDialogueManager.cs
+++ Assets/Scripts/PhoneDialogueManager.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class PhoneDialogueManager : MonoBehaviour
 {
-    public bool inRest;
     private Animation dialogueScroll;
+    public int dialogueCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dialogueCount = 0;
+        dialogueScroll = GetComponent <Animation>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "RestStopTrigger")
+        {
+            PlayAnimation();
+            Debug.Log("Dialogue Animation"+ dialogueCount);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "RestStopTrigger")
+        {
+            dialogueCount++;
+        }
+    }
+
+    void PlayAnimation()
+    {
+        if (dialogueCount == 1)
+        {
+            dialogueScroll.Play("TextScrollAni1");
+        }
+
+        if (dialogueCount == 2)
+        {
+            dialogueScroll.Play("TextScrollAni2");
+        }
+
+        if (dialogueCount == 3)
+        {
+            dialogueScroll.Play("TextScrollAni3");
+        }
     }
 }
