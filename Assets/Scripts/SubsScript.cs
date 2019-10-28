2db92f5527dce1d602d01a7f7b5ea6aba554d22b
Joel Parsons <n10147241@qut.edu.au>
2019-10-28 18:41:09 +1000

    Speedo and subtitles

---
+++ Assets/Scripts/SubsScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubsScript : MonoBehaviour {
+
+    public GameObject textBox;
+
+    void Start() {
+        StartCoroutine(TheSequence());
+
+    }
+
+    IEnumerator TheSequence()
+    {
+        yield return new WaitForSeconds(2);
+        textBox.GetComponent<Text>().text = "Phone Ringing";
+        yield return new WaitForSeconds(2);
+        textBox.GetComponent<Text>().text = "Phone Ringing";
+        yield return new WaitForSeconds(4);
+        textBox.GetComponent<Text>().text = "Hey Just letting you know that i've fueled up and finally left the office";
+        yield return new WaitForSeconds(4);
+        textBox.GetComponent<Text>().text = "OK cool, long day then. How long till you get home?";
+    }
+}
